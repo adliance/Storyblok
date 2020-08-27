64d56ecbcae3356da33bb435f074b4fc36ecdb7a
Hannes Sachsenhofer <hannes.sachsenhofer@adliance.net>
2020-08-27 09:54:40 +0200

    Add new helper class for the default storyblok asset field type.

---
+++ src/Saxx.Storyblok/StoryblokAsset.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Saxx.Storyblok
+{
+    public class StoryblokAsset
+    {
+        [JsonPropertyName("id")] public string? Id { get; set; }
+        [JsonPropertyName("filename")] public string? Url { get; set; }
+        [JsonPropertyName("fieldtype")] public string? FieldType { get; set; }
+        [JsonPropertyName("alt")] public string? Alt { get; set; }
+        [JsonPropertyName("name")] public string? Name { get; set; }
+        [JsonPropertyName("title")] public string? Title { get; set; }
+        [JsonPropertyName("copyright")] public string? Copyright { get; set; }
+    }
+}

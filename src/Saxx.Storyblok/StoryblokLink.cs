a368e221835a480252cf5c51812f3f1e4a8e9372
Hannes Sachsenhofer <hannes.sachsenhofer@adliance.net>
2020-09-12 15:18:40 +0200

    Improve the Link class for handling storyblok links.

---
+++ src/Saxx.Storyblok/StoryblokLink.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Saxx.Storyblok
+{
+    public class StoryblokLink
+    {
+        [JsonPropertyName("id")] public string? Id { get; set; }
+        [JsonPropertyName("url")] public string? Value { get; set; }
+        [JsonPropertyName("linktype")] public string? LinkType { get; set; }
+        [JsonPropertyName("fieldtype")] public string? FieldType { get; set; }
+        [JsonPropertyName("cached_url")] public string? CachedValue { get; set; }
+
+        public string? Url
+        {
+            get
+            {
+                var url = string.IsNullOrWhiteSpace(CachedValue) ? Value : "/" + CachedValue;
+                if (url != null && url.StartsWith("//"))
+                {
+                    url = url.Substring(1);
+                }
+
+                return url;
+            }
+        }
+    }
+}

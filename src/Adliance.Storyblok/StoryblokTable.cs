df689f72457d4f14221d0e88967c8cf5ae80c954
Philipp Burndorfer <philipp.burndorfer@adliance.net>
2021-05-12 15:39:07 +0200

    Adding support for Storyblok tables.

---
+++ src/Adliance.Storyblok/StoryblokTable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Adliance.Storyblok
+{
+    public class StoryblokTable
+    {
+        [JsonPropertyName("_uid")] public Guid Uuid { get; set; }
+        [JsonPropertyName("fieldtype")] public string? FieldType { get; set; }
+        [JsonPropertyName("thead")] public TableHead[]? TableHeaderColumns { get; set; }
+        [JsonPropertyName("tbody")] public TableRow[]? TableBody { get; set; }
+        [JsonPropertyName("_editable")] public string? Editable { get; set; }
+    }
+
+    public class TableHead
+    {
+        [JsonPropertyName("_uid")] public Guid Uuid { get; set; }
+        [JsonPropertyName("value")] public Markdown? Value { get; set; }
+        [JsonPropertyName("_editable")] public string? Editable { get; set; }
+    }
+
+    public class TableRow
+    {
+        [JsonPropertyName("_uid")] public Guid Uuid { get; set; }
+        [JsonPropertyName("body")] public TableCol[]? TableColumns { get; set; }
+        [JsonPropertyName("_editable")] public string? Editable { get; set; }
+    }
+    
+    public class TableCol
+    {
+        [JsonPropertyName("_uid")] public Guid Uuid { get; set; }
+        [JsonPropertyName("value")] public Markdown? Value { get; set; }
+        [JsonPropertyName("_editable")] public string? Editable { get; set; }
+    }
+}
